00a4157ce3ea085618e2510f6615e9eb848ccf74
Sam Hou <sam@samhou.top>
2025-09-25 17:23:00 +0800

    feat: Add basic windows layout

---
+++ VNGod/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using HandyControl;
+
+namespace VNGod
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : HandyControl.Controls.Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void gameList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void rescanButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void addNewGameButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void refreshInfoButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void openGameFolderButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void playButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void editGameButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void bangumiButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void vndbButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void settingsButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void syncButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
